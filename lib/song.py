9eb8d42dfaf300b9fbe3fd97dc7e62feb85aebcb
ryanmakori <ryanndigitalsolutionz@gmail.com>
2026-06-22 11:51:54 +0300

    fixed the variable to artist_count

--- lib/song.py
+++ lib/song.py
@@ -3,7 +3,7 @@ class Song:
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -14,7 +14,7 @@ class Song:
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
         Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
+        Song.add_to_artist_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
@@ -35,7 +35,7 @@ class Song:
         cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
 
     @classmethod
-    def add_to_artists_count(cls, artist):
+    def add_to_artist_count(cls, artist):
         if artist not in cls.artist_count:
             cls.artist_count[artist] = 0
         cls.artist_count[artist] += 1
